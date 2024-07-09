fab8187af12b6e2cb7ea50e47c051dfb10f5a16b
ErnestoTSantos <ernesto.terra2003@gmail.com>
2024-07-08 23:35:19 -0300

    feat: create schedule

--- pampulha/apps/schedule/views.py
+++ pampulha/apps/schedule/views.py
@@ -9,20 +9,3 @@ from pampulha.apps.schedule.serializer import calendarSerializer
 class CalendarViewset(viewsets.ModelViewSet):
     serializer_class = calendarSerializer
     http_method_names = ["get", "post", "patch", "delete"]
-
-    def post(self, request, *args, **kwargs):
-        user = validate_user_token(request)
-        if not user:
-            return Response({"detail": user}, status=status.HTTP_401_UNAUTHORIZED)
-
-        data = request.data
-        data.update(
-            {
-                "psychologist": user
-            }
-        )
-        serializer = calendarSerializer(data=data)
-        serializer.is_valid(raise_exception=True)
-        serializer.create(validated_data=serializer.validated_data)
-
-        return Response(status=status.HTTP_201_CREATED)
