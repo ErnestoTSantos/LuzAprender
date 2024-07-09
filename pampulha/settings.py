ac4ce9e3e252082f8b7f00986972a21ca93d5adc
ErnestoTSantos <ernesto.terra2003@gmail.com>
2024-07-08 23:20:43 -0300

    feat: adjust heroku configurations

--- pampulha/settings.py
+++ pampulha/settings.py
@@ -17,7 +17,9 @@ from decouple import config
 from dj_database_url import parse as dburl
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 
 # Quick-start development settings - unsuitable for production
